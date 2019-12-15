3004d5ae0d9d8e559cd0d9962b6670eacbe75db3
unknown <andy.lee1874@gmail.com>
2019-12-16 02:48:48 +0800

    内容页面开发

--- js/ybd.js
+++ js/ybd.js
@@ -7,4 +7,29 @@ window.onload = function() {
         slidesPerView: 3,
         spaceBetween: 120
     })
+
+    window.onscroll = function() {
+        var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+
+        // view 2/3级导航处理
+        if ($('#view-nav-fixed')) {
+            var fixedNavHeaght = $('#view-nav-fixed').height()
+            $('#view-nav-fixed-wrap').css('height', fixedNavHeaght + 'px')
+            if (scrollTop >= 80) {
+                $('#view-nav-fixed').addClass('nav-fixed')
+            } else {
+                $('#view-nav-fixed').removeClass('nav-fixed')
+            }
+        }
+
+        // 首页头部样式处理
+
+        if (scrollTop == 0 && $('#index-header')) {
+            $('#index-header').removeClass('header-bg-black')
+        } else {
+            $('#index-header').addClass('header-bg-black')
+        }
+
+
+    }
 }
